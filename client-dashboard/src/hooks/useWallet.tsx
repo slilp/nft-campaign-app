2f54827842f3cf8ba62bce05ebb954e9acd9534d
slil puangpoom <slil.puangpoom@gmail.com>
2022-05-29 19:41:43 +0700

    change network to polygon

--- client-dashboard/src/hooks/useWallet.tsx
+++ client-dashboard/src/hooks/useWallet.tsx
@@ -8,7 +8,7 @@ interface UseWalletProps {
 
 function useWallet({ refresh, wallet }: UseWalletProps) {
   const [balance, setBalance] = useState<number>(0);
-  const RPC_URL = "https://data-seed-prebsc-1-s2.binance.org:8545";
+  const RPC_URL = "https://polygon-rpc.com";
   const provider = new StaticJsonRpcProvider(RPC_URL);
 
   useEffect(() => {
